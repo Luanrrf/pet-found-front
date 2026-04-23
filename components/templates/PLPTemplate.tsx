074bde7c12f492f3663a1ffeff18316e7594cd67
Luan Ribeiro <Luanrrf@yahoo.com.br>
2026-04-23 19:07:30 -0300

    feat: changing button color

--- components/templates/PLPTemplate.tsx
+++ components/templates/PLPTemplate.tsx
@@ -11,7 +11,11 @@ export function PLPTemplate() {
     <div>
       {openFilter && <FilterContainer setOpenFilter={setOpenFilter} />}
       <div>
-        <Button type="button" onClick={() => setOpenFilter(!openFilter)}>
+        <Button
+          type="button"
+          onClick={() => setOpenFilter(!openFilter)}
+          className="bg-white text-orange-500 rounded-xl px-4 py-2"
+        >
           <FilterIcon />
         </Button>
       </div>
